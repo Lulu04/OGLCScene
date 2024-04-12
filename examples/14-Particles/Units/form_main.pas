9f7ccf51d31697e10653bbc75dc767a7c58953ac
Lulu <cacameur@gmx.fr>
2024-04-12 21:20:59 +0200

    Added example 14

---
+++ examples/14-Particles/Units/form_main.pas
@@ -0,0 +1,114 @@
+unit form_main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Dialogs, Buttons, ExtCtrls, StdCtrls,
+  BGRABitmap, BGRABitmapTypes,
+  OpenGLContext, OGLCScene,
+  u_common;
+
+type
+
+  { TFormMain }
+
+  TFormMain = class(TForm)
+    CheckBox1: TCheckBox;
+    OpenGLControl1: TOpenGLControl;
+    Panel1: TPanel;
+    Timer1: TTimer;
+    procedure CheckBox1Change(Sender: TObject);
+    procedure FormCloseQuery(Sender: TObject; var {%H-}CanClose: boolean);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+    procedure FormKeyUp(Sender: TObject; var Key: Word; Shift: TShiftState);
+    procedure Timer1Timer(Sender: TObject);
+  private
+    procedure LoadCommonData;
+    procedure FreeCommonData;
+    procedure ProcessApplicationIdle(Sender: TObject; var Done: Boolean);
+  public
+  end;
+
+var
+  FormMain: TFormMain;
+
+implementation
+uses screen_demo;
+{$R *.lfm}
+
+{ TFormMain }
+
+procedure TFormMain.FormCreate(Sender: TObject);
+begin
+  FScene := TOGLCScene.Create(OpenGLControl1, -1);
+  FScene.LayerCount := LAYER_COUNT;
+  FScene.CreateLogFile(Application.Location+'scene.log', True);
+
+  FScene.OnLoadCommonData := @LoadCommonData;
+  FScene.OnFreeCommonData := @FreeCommonData;
+
+  Application.OnIdle := @ProcessApplicationIdle;
+end;
+
+procedure TFormMain.FormDestroy(Sender: TObject);
+begin
+  FScene.Free;
+  FScene := NIL;
+end;
+
+procedure TFormMain.FormCloseQuery(Sender: TObject; var CanClose: boolean);
+begin
+  Timer1.Enabled := FALSE;
+end;
+
+procedure TFormMain.CheckBox1Change(Sender: TObject);
+begin
+  if ScreenDemo = NIL then exit;
+
+  if Sender = CheckBox1 then ScreenDemo.ShipPE.ParticlesPosRelativeToEmitterPos := CheckBox1.Checked;
+end;
+
+procedure TFormMain.FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+begin
+  FScene.ProcessOnKeyDown(Key, Shift);
+end;
+
+procedure TFormMain.FormKeyUp(Sender: TObject; var Key: Word; Shift: TShiftState);
+begin
+  FScene.ProcessOnKeyUp(Key, Shift);
+end;
+
+procedure TFormMain.Timer1Timer(Sender: TObject);
+var pCount: integer;
+begin
+  pCount := 0;
+  if ScreenDemo <> NIL then pCount := ScreenDemo.ParticleCount;
+  Caption := Format('OGLCScene - Scene layers   -   (%d,%d) - %d FPS  -  %d sprites  - %d particles',
+  [FScene.Width, FScene.Height, FScene.FPS, FScene.SurfaceCount, pCount]);
+end;
+
+procedure TFormMain.LoadCommonData;
+begin
+  // we create the single screen of our example
+  ScreenDemo := TScreenDemo.Create;
+  FScene.RunScreen(ScreenDemo, False);
+end;
+
+procedure TFormMain.FreeCommonData;
+begin
+  FreeAndNil(ScreenDemo);
+end;
+
+procedure TFormMain.ProcessApplicationIdle(Sender: TObject; var Done: Boolean);
+begin
+  FScene.DoLoop;
+  Done := FALSE;
+end;
+
+
+end.
+
