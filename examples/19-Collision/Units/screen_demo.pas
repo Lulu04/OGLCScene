de2c25afd1bbebddc8e0ae000233d3353e60d4ab
Lulu <cacameur@gmx.fr>
2024-04-16 21:12:57 +0200

    Added example 19

---
+++ examples/19-Collision/Units/screen_demo.pas
@@ -0,0 +1,151 @@
+unit screen_demo;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils,
+  BGRABitmap, BGRABitmapTypes,
+  OGLCScene,
+  u_common, u_sprite_def;
+
+type
+
+TGameState = (gsRunning);
+
+{ TScreenDemo }
+
+TScreenDemo = class(TScreenTemplate)
+private
+  FAtlas: TOGLCTextureAtlas; // we need an atlas to pack all images in a single texture
+  // font to draw text
+  FTexFont: TTexturedFont;
+  // textures used by the game
+  FtexStar: PTexture;
+
+  FShip: TPlayerShip;       // the sprite for the ship
+private
+  FGameState: TGameState;
+  FStarTimeAccu,
+  FMeteorTimeAccu: single;
+public
+  procedure CreateObjects; override;
+  procedure FreeObjects; override;
+  procedure Update(const AElapsedTime: single); override;
+
+  property GameState: TGameState read FGameState write FGameState;
+end;
+
+var ScreenDemo: TScreenDemo = NIL;
+
+implementation
+uses Forms, LCLType;
+
+{ TScreenDemo }
+
+procedure TScreenDemo.CreateObjects;
+var path: string;
+  ima: TBGRABitmap;
+  zoom: single;
+  fd: TFontDescriptor;
+  i: integer;
+begin
+  // we create an atlas at run time to ensure all images are in the same OpenGL texture -> optimization
+  FAtlas := FScene.CreateAtlas;
+  // to avoid texture artifacts, we want all image in the packed texture separated by 1 pixel
+  FAtlas.Spacing := 1;
+
+  fd.Create('Arial', FScene.ScaleDesignToScene(20), [], BGRA(0,0,0));
+  FtexFont := FAtlas.AddTexturedFont(fd, SIMPLELATIN_CHARSET + ASCII_SYMBOL_CHARSET);
+
+  path := PathToDataFolder;
+  FtexStar := FAtlas.AddFromSVG(path+'SpaceStar.svg', -1, Round(FScene.Height/100));
+
+  // on a 96 PPI screen we choose to have sprites with half of their size in InkScape
+  zoom := FScene.ScaleDesignToSceneF(0.5);
+  FAtlas.AddFromSVG(path+'SpaceShip.svg', Round(294*zoom), -1);
+  FAtlas.AddFromSVG(path+'SpaceShipRadar.svg', Round(50*zoom), -1);
+  FAtlas.AddFromSVG(path+'SpaceShipCanon.svg', Round(41*zoom), -1);
+  FAtlas.AddFromSVG(path+'SpaceShipLaser.svg', Round(22*zoom), -1);
+
+  FAtlas.AddFromSVG(path+'SpaceMeteor.svg', Round(327*zoom), -1);
+
+  // used by the particle emitter for the ship engine.
+  FAtlas.Add(path+'Dust_particle.png');
+  // used by the particle emitter for the meteor explosion.
+  FAtlas.Add(path+'cloud128x128.png');
+
+  FAtlas.TryToPack;
+  FAtlas.Build;    // here the atlas is built and all individuals textures are initialized as part of the
+                   // whole atlas texture.
+
+  // for the example, we save the packed image just to see how the atlas work.
+  ima := FAtlas.GetPackedImage(True, True);
+  ima.SaveToFile(path+'atlas.png');
+  ima.Free;
+  // free some memory because we no longer need individual images
+  FAtlas.FreeItemImages;
+
+  // ship creation
+  FShip := TPlayerShip.Create(FAtlas);
+
+  // randomly fill the scene with stars
+  for i:=0 to 400 do
+    with TStar.Create(FtexStar) do
+      Y.Value := Random(FScene.Height-Height);  // set a random vertical coordinate
+
+  GameState := gsRunning;
+end;
+
+procedure TScreenDemo.FreeObjects;
+begin
+  FScene.ClearAllLayer;    // kill all surfaces on all layer
+  FreeAndNil(FAtlas);
+end;
+
+procedure TScreenDemo.Update(const AElapsedTime: single);
+var delta: single;
+  i: integer;
+begin
+  inherited Update(AElapsedTime);
+
+  case GameState of
+
+    gsRunning: begin
+      // check if player press Right/Left arrow
+      delta := FScene.Width*0.2*AElapsedTime;
+      if FScene.KeyState[VK_LEFT] then FShip.Move(-delta)
+        else if FScene.KeyState[VK_RIGHT] then FShip.Move(delta);
+
+      // check if player press SHIFT
+      if FScene.KeyState[VK_SHIFT] then FShip.Shoot;
+
+      // check collision between the laser (in LAYER_LASER) and the meteors (in LAYER_METEOR)
+      for i:=0 to FScene.Layer[LAYER_LASER].SurfaceCount-1 do
+        TLaserBullet(FScene.Layer[LAYER_LASER].Surface[i]).DoCollisionTestWithMeteor;
+
+      // check collision between player ship and meteors
+      if FShip.DoCollisionTestWithMeteor then;
+
+      // meteor creation
+      FMeteorTimeAccu := FMeteorTimeAccu + AElapsedTime;
+      if FMeteorTimeAccu > 0.5 then begin
+        FMeteorTimeAccu := 0.0;
+        TMeteor.Create(FAtlas);
+      end;
+
+      // stars creation
+      FStarTimeAccu := FStarTimeAccu + AElapsedTime;
+      if FStarTimeAccu > 0.01 then begin
+        FStarTimeAccu := 0.0;
+        TStar.Create(FtexStar);
+      end;
+    end;
+
+  end;//case
+end;
+
+
+end.
+
